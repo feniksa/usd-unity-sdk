d13e7dda0640d59402f8c29339bff8c431890511
Maksym Sditanov <msditanov@200volts.com>
2020-09-28 20:05:04 +0200

    Add to install.cmd installation of UsdCs.dll

--- bin/install.cmd
+++ bin/install.cmd
@@ -8,6 +8,8 @@ copy /y "src\USD.NET.Unity\bin\x64\Release\USD.NET.Unity.pdb" "inst\"
 copy /y "src\USD.NET.Unity\bin\x64\Release\USD.NET.Unity.XML" "inst\"
 copy /y "src\UsdCs\x64\Release\UsdCs.dll" "inst\"
 copy /y "src\UsdCs\x64\Release\UsdCs.pdb" "inst\"
-
+copy /y "src\USDIPC\x64\Release\USDIPC.dll" "inst\"
+copy /y "src\USDIPC\x64\Release\USDIPC.pdb" "inst\"
 xcopy /Y "inst\USD.NET.*" "package\com.unity.formats.usd\Runtime\Plugins\"
 xcopy /Y "inst\UsdCs.*" "package\com.unity.formats.usd\Runtime\Plugins\x86_64\"
+
